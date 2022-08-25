6dedd176cede99dbe0bd511a5b44ede732a0876d
keithCuniah <88380932+keithCuniah@users.noreply.github.com>
2022-08-25 23:20:16 +0200

    format

--- client/src/App.vue
+++ client/src/App.vue
@@ -35,7 +35,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 #app {
   display: flex;
   flex-direction: column;
@@ -45,9 +45,11 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
 nav {
   padding: 30px;
 }
+
 nav a {
   font-weight: bold;
   color: #2c3e50;
@@ -55,6 +57,7 @@ nav a {
   padding: 10px;
   border-radius: 4px;
 }
+
 nav a.router-link-exact-active {
   color: white;
   background: crimson;
