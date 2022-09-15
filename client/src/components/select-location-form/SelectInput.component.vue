9d2e61682f3e377febf37b92e4ccb8133fd7613d
keithCuniah <88380932+keithCuniah@users.noreply.github.com>
2022-09-15 13:37:06 +0200

    replace script with script setup pattern

--- client/src/components/select-location-form/SelectInput.component.vue
+++ client/src/components/select-location-form/SelectInput.component.vue
@@ -20,63 +20,61 @@
   </q-select>
 </template>
 
-<script lang="ts">
-import { ref, toRefs, watch } from "vue";
+<script lang="ts" setup>
+import { PropType, ref, toRefs, watch } from "vue";
 
-export default {
-  props: {
-    options: {
-      type: Object,
-      required: true,
-    },
-    defaultOption: {
-      type: Object,
-    },
-    optionValue: {
-      type: String,
-      required: true,
-    },
-    optionLabel: {
-      type: String,
-      required: true,
-    },
-    isDisabled: { type: Boolean, required: false, default: () => true },
-    label: {
-      type: String,
-      required: true,
-    },
+const props = defineProps({
+  options: {
+    type: Array,
+    required: true,
   },
-  setup(props: any, context: any) {
-    const { options, optionLabel, defaultOption } = toRefs(props);
-    const optionSelected = ref<typeof defaultOption | null>(null);
-    const filteredOptions = ref<typeof options[]>([]);
-
-    const filterOptions = (searchInput: string, update: Function) => {
-      if (searchInput === "") {
-        update(() => {
-          filteredOptions.value = options.value;
-        });
-        return;
-      }
-
-      update(() => {
-        const needle = searchInput.toLowerCase();
-        filteredOptions.value = options.value.filter(
-          (option: any) =>
-            option[optionLabel.value].toLowerCase().indexOf(needle) > -1
-        );
-      });
-    };
-
-    watch(defaultOption, () => {
-      optionSelected.value = defaultOption.value;
-    });
-    watch(optionSelected, () => {
-      context.emit("onSelect", optionSelected);
-    });
-    return { filteredOptions, filterOptions, optionSelected };
+  defaultOption: {
+    type: null as unknown as PropType<object | null>,
+    required: true,
   },
+  optionValue: {
+    type: String,
+    required: true,
+  },
+  optionLabel: {
+    type: String,
+    required: true,
+  },
+  isDisabled: { type: Boolean, required: false, default: () => true },
+  label: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["onSelect"]);
+
+const { options, optionLabel, defaultOption } = toRefs(props);
+const optionSelected = ref<typeof defaultOption | null>(null);
+const filteredOptions = ref<unknown>([]) as typeof options;
+const filterOptions = (searchInput: string, update: Function) => {
+  if (searchInput === "") {
+    update(() => {
+      filteredOptions.value = options.value;
+    });
+    return;
+  }
+
+  update(() => {
+    const needle = searchInput.toLowerCase();
+    filteredOptions.value = options.value.filter(
+      (option: any) =>
+        option[optionLabel.value].toLowerCase().indexOf(needle) > -1
+    );
+  });
 };
+
+watch(defaultOption, () => {
+  optionSelected.value = defaultOption.value;
+});
+watch(optionSelected, () => {
+  emit("onSelect", optionSelected);
+});
 </script>
 
 <style lang="scss" scoped></style>
